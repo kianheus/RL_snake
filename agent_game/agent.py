9d9cae2682be1765feb6dab7328abfb4dd877416
kianheus <Kianheus@gmail.com>
2025-11-26 15:38:01 +0100

    Fix NN call

--- agent_game/agent.py
+++ agent_game/agent.py
@@ -100,7 +100,7 @@ class Agent():
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype = torch.float)
-            prediction = self.model(state0)
+            prediction = self.model.forward(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
         print("final move: ", final_move)
