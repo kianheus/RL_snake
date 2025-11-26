24b5e946684fd64d401087e28c0e6dd961fe724a
kianheus <Kianheus@gmail.com>
2025-11-26 15:38:53 +0100

    Remove IPython for VSCode compatibility

--- agent_game/plotter.py
+++ agent_game/plotter.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
-from IPython import display
 
 plt.ion()
+fig, ax = plt.subplots()
+
+plt.show(block=False)
 
 
 def plot(scores, mean_scores):
-    display.clear_output(wait=True)
-    display.display(plt.gcf())
-    plt.clf()
-    plt.title("Training...")
-    plt.xlabel("Number of games")
-    plt.ylabel("Score")
-    plt.plot(scores)
-    plt.plot(mean_scores)
-    plt.ylim(ymin=0)
-    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    ax.clear()
+    ax.set_title("Training...")
+    ax.set_xlabel("Number of games")
+    ax.set_ylabel("Score")
+    ax.plot(scores)
+    ax.plot(mean_scores)
+    ax.set_ylim(ymin=0)
+    ax.text(len(scores)-1, scores[-1], str(scores[-1]))
+    ax.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    
+    fig.canvas.draw()
+    fig.canvas.flush_events()
