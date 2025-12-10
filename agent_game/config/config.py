b93e108eb6dc896f0ff050d94c08c4b312840106
kianheus <Kianheus@gmail.com>
2025-12-10 11:42:57 +0100

    Add two tabs to separate settings

--- agent_game/config/config.py
+++ agent_game/config/config.py
@@ -7,6 +7,8 @@ class MainWindow(QtW.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        """     General window settings     """
+
         window_x = 150
         window_y = 150
 
@@ -16,6 +18,26 @@ class MainWindow(QtW.QMainWindow):
         self.setWindowTitle("RL Snake settings")
         self.setFixedSize(QtC.QSize(window_width, window_height))
 
+        """     Tabs     """
+        tabs = QtW.QTabWidget()
+        tabs.setTabPosition(QtW.QTabWidget.TabPosition.North)
+        tabs.setMovable(True)
+
+
+
+        """     Tab Main     """
+        placeholder1 = QtW.QWidget()
+
+
+        """     Tab Advanced     """
+        placeholder2 = QtW.QWidget()
+
+
+
+        tabs.addTab(placeholder1, "Main")
+        tabs.addTab(placeholder2, "Advanced")
+
+        self.setCentralWidget(tabs)
 
 
 app = QtW.QApplication([])
