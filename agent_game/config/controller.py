20f2166323c98ed69c184e81bb969cad532a377a
kianheus <Kianheus@gmail.com>
2025-12-16 17:17:20 +0100

    small controller init() changes

--- agent_game/config/controller.py
+++ agent_game/config/controller.py
@@ -6,12 +6,13 @@ from config.model import AgentConfig
 
 class ConfigController(QtC.QObject):
     config_changed = QtC.pyqtSignal(AgentConfig)
+    profile_changed = QtC.pyqtSignal(str)
 
-    def __init__(self, profile_repository: ProfileRepository):
+    def __init__(self, pr: ProfileRepository):
         super().__init__()
-        self.pr = profile_repository
-        self.profile = self.pr.startup_profile
-        self.config = self.pr.load_from_profile(self.profile)
+        self.pr = pr
+        self.active_profile = self.pr.startup_profile
+        self.config = self.pr.load_from_profile(self.active_profile)
 
 
     def set_agent_type(self, agent_type: str):
