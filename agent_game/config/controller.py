3f4edd7470bd4909c6cd9fb6d2a867b0b8428d9a
kianheus <Kianheus@gmail.com>
2025-12-16 17:17:32 +0100

    Add basic profile methods to controller

--- agent_game/config/controller.py
+++ agent_game/config/controller.py
@@ -16,4 +16,29 @@ class ConfigController(QtC.QObject):
 
 
     def set_agent_type(self, agent_type: str):
-        self.config
+        self.config.agent_type = agent_type
+        if agent_type != "Ego":
+            self.config.occupance_size = 0
+        self.config_changed.emit(self.config)
+
+    def switch_profile(self, profile_name: str):
+        self.active_profile = profile_name
+        self.config = self.pr.load_from_profile(profile_name)
+        self.profile_changed.emit(profile_name)
+        self.config_changed.emit(self.config)
+
+    #def set_active_profile(self, profile_name):
+    #    self.active_profile = profile_name
+    #    self.config = self.pr.load_from_profile(profile_name)
+    
+    def create_profile(self, profile_name: str):
+        # Check if any new profile name was entered
+        if not profile_name:
+            raise ValueError("Profile name cannot be empty.")
+        
+        # Avoid adding duplicate profiles
+        if profile_name in self.profiles:
+            raise ValueError(f"A profile named '{profile_name}' already exists.")
+        
+        self.pr.save_profile(profile_name, self.config)
+        self.switch_profile(profile_name)
