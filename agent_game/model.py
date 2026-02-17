3180776d3e7c3b14db1f698226fe5b0324bce6c5
kianheus <Kianheus@gmail.com>
2026-02-17 16:25:47 +0100

    Save model data to proper results directory

--- agent_game/model.py
+++ agent_game/model.py
@@ -24,8 +24,8 @@ class Linear_QNet(nn.Module):
         x = self.layers[-1](x)
         return x
     
-    def save(self, file_name="model.pth"):
-        model_folder_path = "./models"
+    def save(self, save_path, file_name="model.pth"):
+        model_folder_path = save_path
         os.makedirs(model_folder_path, exist_ok=True)
 
         file_name = os.path.join(model_folder_path, file_name)
